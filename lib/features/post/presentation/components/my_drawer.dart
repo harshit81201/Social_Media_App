089d92f1eb20d91bb8aff8a3514e645f9e176223
harshit81201 <harshit81201@gmail.com>
2025-03-03 04:25:20 +0530

    updated drawer

--- lib/features/post/presentation/components/my_drawer.dart
+++ lib/features/post/presentation/components/my_drawer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:social_media_app/features/post/presentation/components/my_drawer_tile.dart';
 
 class MyDrawer extends StatelessWidget {
   const MyDrawer({super.key});
@@ -7,15 +8,34 @@ class MyDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(
       backgroundColor: Theme.of(context).colorScheme.surface,
-      child: const SafeArea(
+      child: SafeArea(
         child: Column(
           children: [
-            SizedBox(height: 50),
-            Icon(
+            const SizedBox(height: 50),
+            const Icon(
               Icons.person,
               size: 100,
               color: Colors.grey,
             ),
+
+            //home tile
+            MyDrawerTile(
+              title: 'H O M E',
+              icon: Icons.home,
+              onTap: () {},
+            ),
+
+
+            //profile tile
+
+
+            //search tile
+
+
+            //settings tile
+
+
+            //logout tile
           ],
         ),
       ),
